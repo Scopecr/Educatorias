c4a88510095e1ded2a3a30b5b152b0b9ddd74c4f
Walter Carrion <87916815+Scopecr@users.noreply.github.com>
2026-08-12 10:46:17 -0400

    Refactor code structure for improved readability and maintainability

--- Actividades/js/gallery-data.js
+++ Actividades/js/gallery-data.js
@@ -11,7 +11,7 @@
 //
 // Ejemplo (déjalo comentado como referencia o cópialo y edítalo):
 //
-// const ALBUMS = [
+// window.ALBUMS = [
 //   {
 //     id: 'dia-de-lectura',
 //     title: 'Día de Lectura',
@@ -20,4 +20,75 @@
 //   },
 // ];
 
-const ALBUMS = [];
+window.ALBUMS = [
+  {
+    id : 'Campamento-de-verano-2026',
+    title : 'Campamento de verano 2026',
+    date : 'Julio 2026',
+    images : [
+      'Seminario jiu jitsu 1.JPEG',
+      'Grupal enbeleco.jpg',
+      'Mapa Puerto Rico.jpg',
+      'Peliculas.jpg',
+      'Pool day.jpg',
+      'Untitled.jpg',
+      'embeleco 2.jpg',
+      'enfermera.jpg',
+      'escurcion vaqueria.jpg',
+      'grupal comida de perros.jpg'
+    ]
+  },
+  {
+    id : 'Egg-hunt-2026',
+    title : 'Egg Hunt 2026',
+    date : 'Marzo 2026',
+    images : [
+      'Egg-Hunt-1.jpg',
+      'Egg-Hunt-2.jpg',
+      'Egg-Hunt-3.jpg',
+      'Egg-Hunt-4.jpg',
+      'Egg-Hunt-5.jpg',
+      'Egg-Hunt-6.jpg',
+      'Egg-Hunt-7.jpg',
+      'Egg-Hunt-8.jpg',
+      'Egg-Hunt-9.jpg',
+      'Egg-Hunt-10.jpg',
+      'Egg-Hunt-11.jpg',
+      'Egg-Hunt-12.jpg',
+      'Egg-Hunt-13.jpg',
+      'Egg-Hunt-14.jpg',
+      'Egg-Hunt-15.jpg'
+    ]
+  },
+  {
+    id : 'Valentines-Cupcakes-2026',
+    title : 'Valentine\'s Day Cupcakes 2026',
+    date : 'Febrero 2026',
+    images : [
+      'valentines-1.jpg',
+      'valentines-2.jpg',
+      'valentines-3.jpg',
+      'valentines-4.jpg',
+      'valentines-5.jpg',
+      'valentines-6.jpg',
+      'valentines-7.jpg'
+    ]
+  },
+  {
+    id : 'Christmas-Cookies-2026',
+    title : 'Christmas Cookies 2025',
+    date : 'Diciembre 2025',
+    images : [
+      'cc-1.jpg',
+      'cc-2.jpg',
+      'cc-3.jpg',
+      'cc-4.jpg',
+      'cc-5.jpg',
+      'cc-6.jpg',
+      'cc-7.jpg',
+      'cc-8.jpg',
+      'cc-9.jpg',
+      'cc-10.jpg'
+    ]
+  }
+];
